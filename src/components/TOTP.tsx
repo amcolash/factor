ccfb9fea89ca4d31ce52978f116021e4bffbcbc7
Andrew McOlash <amcolash@gmail.com>
2024-03-26 21:17:02 -0700

    add magnifying glass icon to search

--- src/components/TOTP.tsx
+++ src/components/TOTP.tsx
@@ -27,7 +27,20 @@ export function TOTP({ userData, userRef }: { userData: UserData; userRef: Docum
         {userData.keys.length === 0 && <div className="text-center text-lg bg-slate-800 p-8 w-full rounded-md">No keys added yet</div>}
         {userData.keys.length > 0 && (
           <div className="w-full max-w-screen-lg">
-            <input className="mb-6" type="search" value={search} placeholder="Search tokens" onChange={(e) => setSearch(e.target.value)} />
+            <div className="relative">
+              <input
+                className="mb-6 pl-10"
+                type="search"
+                value={search}
+                placeholder="Search tokens"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              {/* Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. */}
+              <svg className="fill-slate-400 absolute left-3 top-[11px] h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+              </svg>
+            </div>
+
             <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 sm:gap-6">
               {userData.keys
                 .filter((k) => search.length === 0 || k.name.toLowerCase().includes(search.toLowerCase()))
