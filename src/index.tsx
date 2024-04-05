3e74de4970c4d65580a89d8a066a5b04bb0af118
Andrew McOlash <amcolash@gmail.com>
2024-04-04 20:42:23 -0700

    try to add toast for update notification

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { Buffer } from 'buffer';
 import { createRoot } from 'react-dom/client';
-import { ToastContainer, Zoom } from 'react-toastify';
+import { ToastContainer, Zoom, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { registerSW } from 'virtual:pwa-register';
 
@@ -9,15 +9,6 @@ import { App } from './components/App';
 // Fixes for browser-passworder
 window.global ||= window;
 globalThis.Buffer = Buffer;
-
-// add this to prompt for a refresh
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('New content available. Reload?')) {
-      updateSW();
-    }
-  },
-});
 
 // If a pwa on desktop, resize window a bit smaller
 const isBrowser = matchMedia('(display-mode: browser)').matches;
@@ -28,6 +19,13 @@ if (!isBrowser) {
 // Enable virtual keyboard api
 const nav = navigator as any;
 if (nav.virtualKeyboard) nav.virtualKeyboard.overlaysContent = true;
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    toast.warn('New version available, click to update.', { autoClose: false, onClick: () => updateSW() });
+  },
+});
 
 function Container() {
   return (
