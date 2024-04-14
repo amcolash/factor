2b0550d6c461d5c9ecbc095929a60119cc4f13d5
Andrew McOlash <amcolash@gmail.com>
2024-04-14 12:34:32 -0700

    fix os check

--- src/hooks/useOsType.ts
+++ src/hooks/useOsType.ts
@@ -1,25 +1,25 @@
 export enum OS {
-  Linux,
-  Mac,
-  Windows,
-  iOS,
-  Android,
-  Other,
+  Linux = 'Linux',
+  Mac = 'Mac',
+  Windows = 'Windows',
+  iOS = 'iOS',
+  Android = 'Android',
+  Other = 'Other',
 }
 
 export function useOsType(): OS {
   const userAgent = navigator.userAgent;
 
-  if (userAgent.includes('Mac')) {
+  if (userAgent.includes('iPhone') || userAgent.includes('iPad') || userAgent.includes('iPod')) {
+    return OS.iOS;
+  } else if (userAgent.includes('Android')) {
+    return OS.Android;
+  } else if (userAgent.includes('Mac')) {
     return OS.Mac;
   } else if (userAgent.includes('Win')) {
     return OS.Windows;
   } else if (userAgent.includes('Linux')) {
     return OS.Linux;
-  } else if (userAgent.includes('iPhone') || userAgent.includes('iPad') || userAgent.includes('iPod')) {
-    return OS.iOS;
-  } else if (userAgent.includes('Android')) {
-    return OS.Android;
   }
 
   return OS.Other;
